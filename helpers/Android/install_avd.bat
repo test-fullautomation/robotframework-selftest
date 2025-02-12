d8cfe2396e1fe23cd84020dad7d3a65009c0b234
mar3hc <tri.maiminh@vn.bosch.com>
2025-02-12 15:48:14 +0700

    update bat files to install and load avd

--- helpers/Android/install_avd.bat
+++ helpers/Android/install_avd.bat
@@ -1,16 +1,14 @@
 @echo off
 
-set path=%path%;
-set ANDROID_HOME=%RobotDevtools%\Android
-set EMULATOR=%RobotDevtools%\Android\tools
-set AVD=%ANDROID_HOME%\tools\bin
+set AVD=%RobotDevtools%\Android\tools\bin
 set AVD_NAME=my_avd
 
 REM Check if AVD exists
-"%AVD%"\avdmanager list avd | findstr /C:"Name: %AVD_NAME%" > nul
+"%AVD%\avdmanager" list avd | findstr /C:"Name: %AVD_NAME%" > nul
 if errorlevel 1 (
     REM AVD does not exist, create it
-    start "Install AVD" "%AVD%"\avdmanager create avd -n %AVD_NAME% -k "system-images;android-31;google_apis_playstore;x86_64" --force --device "pixel_xl"
+	"%AVD%\sdkmanager" "system-images;android-31;google_apis_playstore;x86_64"
+    start Install AVD "%AVD%\avdmanager" create avd -n %AVD_NAME% -k "system-images;android-31;google_apis_playstore;x86_64" --force --device "pixel_xl"
 )
 
 @echo on
--- helpers/Android/start_avd.bat
+++ helpers/Android/start_avd.bat
@@ -3,8 +3,9 @@
 set path=%path%;
 set ANDROID_HOME=%RobotDevtools%\Android
 set EMULATOR=%RobotDevtools%\Android\tools
+set ANDROID_SDK_ROOT=%USERPROFILE%\AppData\Local\Android\Sdk
 
 REM Start the emulator
-start "Start Emulator" "%EMULATOR%"\emulator\emulator -avd my_avd -no-snapshot-load
+start "Start Emulator" "%EMULATOR%\emulator" -avd my_avd -no-snapshot-load
 
 @echo on
