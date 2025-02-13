92a920235ebde7e2016f6cff52a63ca6e891920c
mar3hc <tri.maiminh@vn.bosch.com>
2025-02-13 17:32:30 +0700

    update to install android emulator hypervisor driver

--- helpers/Android/install_avd.bat
+++ helpers/Android/install_avd.bat
@@ -2,11 +2,13 @@
 
 set AVD=%RobotDevtools%\Android\tools\bin
 set AVD_NAME=my_avd
+set AEHD=%RobotDevtools%\Android\aehd-windows_v2_2_0
 
 REM Check if AVD exists
 "%AVD%\avdmanager" list avd | findstr /C:"Name: %AVD_NAME%" > nul
 if errorlevel 1 (
     REM AVD does not exist, create it
+    "%AEHD%\silent_install.bat"
     "%AVD%\avdmanager" create avd -n my_avd -k "system-images;android-34;google_apis;x86_64" --force --device "pixel_xl"
 )
 
