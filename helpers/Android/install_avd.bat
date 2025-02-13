d1b1d48a7eb63c5f451818322523153556c1727f
mar3hc <tri.maiminh@vn.bosch.com>
2025-02-13 17:26:45 +0700

    update to install avd

--- helpers/Android/install_avd.bat
+++ helpers/Android/install_avd.bat
@@ -7,8 +7,7 @@ REM Check if AVD exists
 "%AVD%\avdmanager" list avd | findstr /C:"Name: %AVD_NAME%" > nul
 if errorlevel 1 (
     REM AVD does not exist, create it
-	"%AVD%\sdkmanager" "system-images;android-31;google_apis_playstore;x86_64"
-    start Install AVD "%AVD%\avdmanager" create avd -n %AVD_NAME% -k "system-images;android-31;google_apis_playstore;x86_64" --force --device "pixel_xl"
+    "%AVD%\avdmanager" create avd -n my_avd -k "system-images;android-34;google_apis;x86_64" --force --device "pixel_xl"
 )
 
-@echo on
+ @echo on
