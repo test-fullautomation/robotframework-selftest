3d038dff84cb297c864f930273ab75e39781af70
mar3hc <tri.maiminh@vn.bosch.com>
2025-02-26 11:15:25 +0700

    update path

--- helpers/Android/Appium.bat
+++ helpers/Android/Appium.bat
@@ -6,5 +6,5 @@ set path=%path%;
 set ANDROID_HOME=%RobotDevtools%\Android
 set APPIUM_HOME=%RobotNodeJS%
 
-start "Appium" "%RobotAppium%\appium" --allow-insecure=adb_shell --log D:\work\robot_fw_AIO_full\robotframework-selftest\testcases\aiotestlogfiles\appium.log
+start "Appium" "%RobotAppium%\appium" --allow-insecure=adb_shell --log D:\a\RobotFramework_AIO\robotframework-selftest\testcases\aiotestlogfiles\appium.log
 @echo on
