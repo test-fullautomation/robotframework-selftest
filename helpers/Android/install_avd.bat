7519955e2d9f26ce07423ae81403ff5e054a24e2
mar3hc <tri.maiminh@vn.bosch.com>
2025-03-10 10:40:08 +0700

    update install avd without aehd

--- helpers/Android/install_avd.bat
+++ helpers/Android/install_avd.bat
@@ -8,6 +8,5 @@ REM Check if AVD exists
 "%AVD%\avdmanager" list avd | findstr /C:"Name: %AVD_NAME%" > nul
 if errorlevel 1 (
     REM AVD does not exist, create it
-    "%AEHD%\silent_install.bat"
     "%AVD%\avdmanager" create avd -n my_avd -k "system-images;android-34;google_apis;x86_64" --force --device "pixel_xl"
 )
