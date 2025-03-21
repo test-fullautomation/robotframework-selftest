9430d0e3cdf4acf23c007e07f9c28306c47ca7f4
mar3hc <tri.maiminh@vn.bosch.com>
2025-03-21 10:33:15 +0700

    update

--- helpers/Android/start_avd.bat
+++ helpers/Android/start_avd.bat
@@ -1,9 +1,9 @@
 @echo on
 
 set path=%path%;
-set ANDROID_HOME=%RobotDevtools%\Android
-set EMULATOR=%RobotDevtools%\Android\tools
-set ANDROID_SDK_ROOT=%USERPROFILE%\AppData\Local\Android\Sdk
+set ANDROID_HOME=%RobotDevtools%/Android
+set EMULATOR=%RobotDevtools%/Android/tools
+set ANDROID_SDK_ROOT=%USERPROFILE%/AppData/Local/Android/Sdk
 
 REM Start the emulator
-start /min "Start Emulator" "%EMULATOR%\emulator" -avd my_avd -accel on -gpu auto -no-snapshot-load -wipe-data -memory 4096 -cores 4 -no-window -no-boot-anim
+start /min "Start Emulator" "%EMULATOR%/emulator" -avd my_avd -accel on -gpu auto -no-snapshot-load -wipe-data -memory 4096 -cores 4 -no-window -no-boot-anim
