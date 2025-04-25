8e3785b0d5969dc8d957336a6d270c0fe01a0e00
mar3hc <tri.maiminh@vn.bosch.com>
2025-04-25 15:08:03 +0700

    add reason to skip android test in windows

--- testcases/executerobottest.py
+++ testcases/executerobottest.py
@@ -63,8 +63,8 @@ sPython         = CString.NormalizePath(sys.executable)
 sPythonVersion  = sys.version
 
 if sPlatformSystem == "Windows":
-    # nothing specific to do
-    sys.exit(nReturn)
+    # temporarily disabled the test due to an issue with the Android Virtual Device
+    sys.exit()
 elif sPlatformSystem == "Linux":
     # nothing specific to do
     pass
